ae5a4bc7c57031839d9658ab74995a1c67052537
raikkon <raikkonlin@gmail.com>
2015-10-06 19:11:23 +0800

    Initial Commit

---
+++ RandomCompareSum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomCompareSum
+//
+//  Created by LINCHUNGYAO on 2015/10/6.
+//  Copyright © 2015年 LINCHUNGYAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RandomCompareSum/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RandomCompareSum
+//
+//  Created by LINCHUNGYAO on 2015/10/6.
+//  Copyright © 2015年 LINCHUNGYAO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
